68420af01a4ea01bff2fe0f54ca1de8b71e5db20
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2017-06-08 13:17:28 +0900

    director & jdk version housekeeping

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CD_VER=$1
-CD_VER=${CD_VER:=2.3.0}
+CD_VER=${CD_VER:=2}
 
 docker build --build-arg CD_VER=${CD_VER} -t kirasoa/cloudera-director-server:${CD_VER} -t kirasoa/cloudera-director-server:latest server
 docker build --build-arg CD_VER=${CD_VER} -t kirasoa/cloudera-director-client:${CD_VER} -t kirasoa/cloudera-director-client:latest client
