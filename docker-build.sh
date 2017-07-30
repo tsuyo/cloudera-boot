beb61cb2bd1616c8f1802c9f735c3628e9948ebd
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2017-07-30 17:20:07 +0900

    integrate docker images into one

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-CD_VER=$1
-CD_VER=${CD_VER:=2}
+CD_VER_FULL=$1
+CD_VER_FULL=${CD_VER_FULL:=2}
+a=( ${CD_VER_FULL//./ } )
+CD_VER=${a[0]}
 
-docker build --build-arg CD_VER=${CD_VER} -t kirasoa/cloudera-director-server:${CD_VER} -t kirasoa/cloudera-director-server:latest server
-docker build --build-arg CD_VER=${CD_VER} -t kirasoa/cloudera-director-client:${CD_VER} -t kirasoa/cloudera-director-client:latest client
+docker build --build-arg CD_VER=${CD_VER} -t kirasoa/cloudera-boot:${CD_VER_FULL} -t kirasoa/cloudera-boot:${CD_VER} -t kirasoa/cloudera-boot:latest .
